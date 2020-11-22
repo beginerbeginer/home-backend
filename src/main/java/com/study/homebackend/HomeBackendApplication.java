1701c0cf3608a3e0b9c351fc5be30501cdf6e080
kumasaka <kumasaka9732@gmal.com>
2020-11-22 11:15:27 +0900

    first commit

---
+++ src/main/java/com/study/homebackend/HomeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.study.homebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/homebackend/HomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.homebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
